610a9fe1ff56ddda874445082b3016cb72d059bc
daviniaaa <daviniaho99@gmail.com>
2023-06-09 15:16:49 +0800

    test dockerfile edit

--- src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
+++ src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
@@ -1,5 +1,8 @@
 package vttp2023.batch3.ssf.frontcontroller.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class FrontController {
 
 	// TODO: Task 2, Task 3, Task 4, Task 6
